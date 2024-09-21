6c0b39cd3d552b184c173b5c5682fc4dc7cc3a90
Retro-Markz <cubedomarco@gmail.com>
2024-09-21 01:38:53 -0700

    start project structure

---
+++ src/Snack.java
@@ -0,0 +1,65 @@
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Snack implements Serializable {
+
+    private static int contadorSnacks = 0;
+    private int idSnack;
+    private String nombre;
+    private double precio;
+
+    public Snack(){
+        this.idSnack = ++Snack.contadorSnacks;
+    }
+
+    public Snacks(String nombre, double precio){
+        this(); //la primer linea es la llamada al constructor vacio
+        this.nombre = nombre;
+        this.precio = precio;
+
+    }
+
+
+    public int getIdSnack() {
+        return this.idSnack;
+    }
+
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Snack{" +
+                "idSnack=" + idSnack +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Snack snack = (Snack) o;
+        return idSnack == snack.idSnack && Double.compare(precio, snack.precio) == 0 && Objects.equals(nombre, snack.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idSnack, nombre, precio);
+    }
+}
