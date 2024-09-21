c957b576ccde06095f0b17f71bda5b8c8fb5a73e
Retro-Markz <cubedomarco@gmail.com>
2024-09-21 01:52:54 -0700

    snacks list done

--- src/Snacks.java
+++ src/Snacks.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snacks {
+
+    private static final List<Snack> snacks;
+
+    //bloque de tipo estatico inicializador
+    static{
+        snacks = new ArrayList<>();
+        snacks.add(new Snack("Papas", 70));
+        snacks.add(new Snack("Refresco", 50));
+        snacks.add(new Snack("Sandwich", 120));
+    }
+
+
+    public static void addSnack(Snack snack){
+        snacks.add(snack);
+    }
+
+    public  static void showSnacks(){
+        var inventarioSnacks = "";
+        for(var snacks: snacks){
+            inventarioSnacks += snacks.toString() + "\n";
+        }
+
+        System.out.println(" ---- Snacks en el inventario ---- ");
+        System.out.println(inventarioSnacks);
+    }
+
+
+    public static List<Snack> getSnacks(){
+        return snacks;
+    }
+
+
+
+
 }
